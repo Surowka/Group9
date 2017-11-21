f1b70842cb13f466dd6a98ef4191b440c43df991
shirmplord <33469486+shirmplord@users.noreply.github.com>
2017-11-21 10:31:22 +0200

    what happened :P
    I don't get GIT anymore

--- ZumoBot.cydsn/ZumoLibrary/Motor.h
+++ ZumoBot.cydsn/ZumoLibrary/Motor.h
@@ -17,6 +17,8 @@ void motor_forward(uint8 speed,uint32 delay);
 /* moving left when l_speed < r_speed or moving right when l_speed > r_speed */
 void motor_turn(uint8 l_speed, uint8 r_speed, uint32 delay);
 
+void motor_turn_float(float l_speed, float r_speed, uint32 delay);
+void motor_forward_us (uint8 speed,uint32 delay);
 /* moving backward */
 void motor_backward(uint8 speed,uint32 delay);
 
